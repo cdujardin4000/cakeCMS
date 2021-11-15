1f322a198200c3904156885c06e74b97f012615d
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2021-11-15 22:38:21 +0100

    c'est cool cake php

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -6,19 +6,35 @@ use Cake\ORM\Table;
 use Cake\Utility\Text;
 // the EventInterface class
 use Cake\Event\EventInterface;
+// the Validator class
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table {
 
     public function initialize(array $config): void{
+        // fonctions qui sont appellées lors de crétion d'un article
         $this->addBehavior('Timestamp');
     }
 
     public function beforeSave(EventInterface $event, $entity, $options){
-
+        // fonction qui formate le titre pour faire le slug qui sert d'identifiant dans le barre d'adresse
         if ($entity->isNew() && !$entity->slug) {
             $sluggedTitle = Text::slug($entity->title);
             // trim slug to maximum length defined in schema
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    public function validationDefault(Validator $validator): Validator{
+        // Ajout des restrictions pour la crétion d'un article
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -1,6 +1,13 @@
 
 <h1>Articles</h1>
-<p><?= $this->Html->link('Add Article', ['action' => 'add']) ?></p>
+<p>
+    <?=
+        $this->Html->link(
+            'Add Article',
+            ['action' => 'add']
+        )
+    ?>
+</p>
 <table>
     <tr>
         <th>Title</th>
@@ -13,13 +20,29 @@
     <?php foreach ($articles as $article): ?>
     <tr>
         <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+            <?=
+                $this->Html->link(
+                    $article->title,
+                    ['action' => 'view', $article->slug]
+                )
+            ?>
         </td>
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
         </td>
         <td>
-            <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+            <?=
+                $this->Html->link(
+                    'Edit',
+                    ['action' => 'edit', $article->slug]
+                )
+            ?>
+            <?= $this->Form->postLink(
+                    'Delete',
+                    ['action' => 'delete', $article->slug],
+                    ['confirm' => 'Are you sure?']
+                )
+            ?>
         </td>
     </tr>
     <?php endforeach; ?>
